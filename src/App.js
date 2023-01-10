29324929438c62890418e6e7b3066f3d3190ad73
Matias Bruna <matibruna6@gmail.com>
2023-01-10 14:39:22 -0300

    Fin del curso (trabajando en este proyecto)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { UseState } from "./components/UseState/index.js";
 import { ClassState } from "./components/ClassState/index.js";
+import { UseReducer } from "./components/UseReducer/index.js";
 
 function App() {
   return (
     <div className="App">
       <UseState name="UseState"/>
       <ClassState name="ClassState"/>
+      <UseReducer name="UseReducer"/>
     </div>
   );
 }
